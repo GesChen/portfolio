3910caab9addc75b89c9dbc700f904456ece8dc2
GesChen <omgtntlol101@gmail.com>
2024-03-22 20:12:30 -0400

    preloader please work i hate servers

--- preload.js
+++ preload.js
@@ -1,17 +1,21 @@
 var extrainfoJson;
 
 (async () => {
-  try {
-    const response = await fetch('extrainfo.json');
-    const json = await response.json();
-    extrainfoJson = json;
+    try {
+        const response = await fetch('extrainfo.json');
+        const json = await response.json();
+        extrainfoJson = json;
+        
+        for (let key in extrainfoJson) {
+            console.log(`loading ${key}`);
+            let location = extrainfoJson[key]['location'];
+            let image = new Image();
+            image.src = location;
 
-    for (let key in extrainfoJson) {
-      let location = extrainfoJson[key]['location'];
-      let image = new Image();
-      image.src = location;
+            await new Promise(resolve => setTimeout(resolve, 100));
+        }
+        console.log("finished loading");
+    } catch (error) {
+        console.error('Error fetching extrainfo.json:', error);
     }
-  } catch (error) {
-    console.error('Error fetching extrainfo.json:', error);
-  }
 })();
